620d733cb3716c33d2fcf3638c1eca0bdbb7e674
Jack Choi <rusiaboy@gmail.com>
2016-03-25 16:53:28 +1300

    Adding the new Game.java file which will be used to run the frame, working the Window.java file made in the testing branch (Soon to be merged into the master branch).

--- Game.java
+++ Game.java
@@ -1,9 +1,26 @@
 package combat;
 
-public class Game {
+import java.awt.Canvas;
 
-	public static void main(String[] args) {
+public class Game extends Canvas implements Runnable{
+	
+	public static final int WIDTH = 1024, HEIGHT = 768;
+	private static final long serialVersionUID = 3818541701527953868L;
+	
+	public Game(){
+		new Window(WIDTH, HEIGHT, "Combat", this);
+	}
+	
+	public synchronized void start(){
 		
+	}
+	
+	public void run(){
+		
+	}
+	
+	public static void main(String args[]) {
+		new Game();
 	}
 
 }
